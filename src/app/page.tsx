65296caac80c9935e66a308faa2ddfbcba28125b
Kotchakorn Wongwisutigul <icekotcha@gmail.com>
2026-06-04 23:26:27 +0700

    feat: refactor layout and components, add Section and Container components, and implement typewriter effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
-import { GitHub, Linkedin, Globe, File } from 'react-feather'
+import { GitHub, Linkedin, Globe } from 'react-feather'
 import Container from '../components/Container'
 import SocialButton from '../components/SocialButton'
-import Typewriter from '../components/Typewriter'
+import Typewriter from '../components/Typewriter/infdex'
+import Section from '@/components/Section'
 
 const ICON_SIZE = 16
 
@@ -26,31 +27,39 @@ const socialLinks = [
 
 const HomePage = () => {
   return (
-    <div>
-      <Container>
-        <Image
-          src='/images/owner.jpg'
-          width={128}
-          height={175}
-          alt='Avatar'
-          className='rounded-full h-32 w-32 object-cover object-[50%_80%] mb-4 border-dashed border-white border-2'
-        />
-        <p className='text-4xl md:text-6xl font-bold p-4'>
-          Hi! this is <Typewriter text="Icegotcha" className="text-purple-800" duration={1800} />.
-        </p>
-        <p className='text-center text-xl p-4'>
-          I&apos;m passionate about learning and exploring new technologies, and
-          enjoy sharing my knowledge and experiences with others.
-        </p>
-        <ul className='list-none mx-auto p-4 flex flex-col sm:flex-row justify-center gap-4'>
-          {socialLinks.map((item, index) => (
-            <li key={index}>
-              <SocialButton {...item} />
-            </li>
-          ))}
-        </ul>
-      </Container>
-    </div>
+    <Container>
+      <Section>
+        <div className='flex flex-col justify-center items-center text-center text-gray-800'>
+          <Image
+            src='/images/owner.jpg'
+            width={128}
+            height={175}
+            loading='eager'
+            alt='Avatar'
+            className='rounded-full h-32 w-32 object-cover object-[50%_80%] mb-4 border-dashed border-white border-2'
+          />
+          <p className='text-4xl md:text-6xl font-bold p-4'>
+            Hi! this is{' '}
+            <Typewriter
+              text='Icegotcha'
+              className='text-purple-800'
+              duration={1800}
+            />
+          </p>
+          <p className='text-center text-xl p-4'>
+            Software Engineer | Lifelong Learner | Writer at Heart | Crafting
+            Ideas into Reality
+          </p>
+          <ul className='list-none mx-auto p-4 flex flex-col sm:flex-row justify-center gap-4'>
+            {socialLinks.map((item, index) => (
+              <li key={index}>
+                <SocialButton {...item} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Section>
+    </Container>
   )
 }
 
